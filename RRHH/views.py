753f9db9d3d3f242cdbdbb9e35b9fa1f543eac7c
trodrigx <tumayquispevega@gmail.com>
2024-10-24 15:57:33 -0500

    ERP

---
+++ RRHH/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render())
